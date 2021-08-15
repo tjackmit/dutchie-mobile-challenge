3fc164aef4106a895599713f85113ae98ac8dfe2
dev <tim.d.fuller@gmail.com>
2021-08-15 12:47:18 -0600

    Tim Fuller - Update code base.

--- lib/app/theme/icons.dart
+++
@@ -1,8 +0,0 @@
-const String SVG_PATH = 'lib/assets/svgs/';
-const String CHALLENGE_SVG = SVG_PATH + 'challenge.svg';
-const String DISPENSARY_SVG = SVG_PATH + 'dispensary.svg';
-const String HIGH_CBD_SVG = SVG_PATH + 'high-cbd.svg';
-const String RESULT_SVG = SVG_PATH + 'result.svg';
-const String INDICA_SVG = SVG_PATH + 'indica.svg';
-const String SATIVA_SVG = SVG_PATH + 'sativa.svg';
-const String HYBRID_SVG = SVG_PATH + 'hybrid.svg';
